c64b56b568d7868a442cf323e888d7137c57ae70
janishiestand <janis_hiestand@hotmail.com>
2023-03-29 12:59:35 +0200

    Comparison Test

--- EFDataAccessLibrary/Interfaces/IGenericRepository.cs
+++ EFDataAccessLibrary/Interfaces/IGenericRepository.cs
@@ -9,7 +9,6 @@ namespace EFDataAccessLibrary.Interfaces
         IEnumerable<T> GetAll();
         IEnumerable<T> Find(Expression<Func<T, bool>> expression);
         void Add(T entity);
-        // void AddRange(IEnumerable<T> entities);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
         Task<List<T>> ReadAllAsync();
